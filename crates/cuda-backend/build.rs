40f29307c19ce8ce44cf8172df6b01c787742e63
Grigorii Alekseev <grigorii@intrinsictech.xyz>
2025-08-15 23:06:41 +0000

    if cuda not available - no build

--- crates/cuda-backend/build.rs
+++ crates/cuda-backend/build.rs
@@ -1,6 +1,12 @@
-use openvm_cuda_builder::CudaBuilder;
+use openvm_cuda_builder::{cuda_available, emit_cuda_cfg_if_available, CudaBuilder};
 
 fn main() {
+    emit_cuda_cfg_if_available();
+    if !cuda_available() {
+        eprintln!("cargo:warning=CUDA is not available");
+        return;
+    }
+
     let common = CudaBuilder::new()
         .include_from_dep("DEP_CUDA_COMMON_INCLUDE")
         .watch("cuda")
--- crates/cuda-common/build.rs
+++ crates/cuda-common/build.rs
@@ -1,9 +1,13 @@
 use std::{env, path::PathBuf};
 
-fn main() {
-    openvm_cuda_builder::emit_cuda_cfg_if_available();
+use openvm_cuda_builder::{cuda_available, emit_cuda_cfg_if_available};
 
-    let include_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("include");
-    println!("cargo:include={}", include_path.display()); // -> DEP_CUDA_COMMON_INCLUDE
-    println!("cargo:rerun-if-changed={}", include_path.display());
+fn main() {
+    emit_cuda_cfg_if_available();
+
+    if cuda_available() {
+        let include_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("include");
+        println!("cargo:include={}", include_path.display()); // -> DEP_CUDA_COMMON_INCLUDE
+        println!("cargo:rerun-if-changed={}", include_path.display());
+    }
 }
