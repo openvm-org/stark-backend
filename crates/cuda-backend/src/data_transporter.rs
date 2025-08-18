893109b88e6feab2d49d3e1799270de5a2169733
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2025-08-18 15:33:13 +0000

    chore: remove unnecessary transpose in matrix assert_eq

--- crates/cuda-backend/src/data_transporter.rs
+++ crates/cuda-backend/src/data_transporter.rs
@@ -134,13 +134,13 @@ pub fn assert_eq_device_matrix<T: Clone + Send + Sync + PartialEq + Debug>(
     assert_eq!(a.height(), b.height());
     assert_eq!(a.width(), b.width());
     assert_eq!(a.buffer().len(), b.buffer().len());
-    let a_host = transport_device_matrix_to_host(a);
-    let b_host = transport_device_matrix_to_host(b);
-    for r in 0..a_host.height() {
-        for c in 0..a_host.width() {
+    let a_host = a.to_host().unwrap();
+    let b_host = b.to_host().unwrap();
+    for r in 0..a.height() {
+        for c in 0..a.width() {
             assert_eq!(
-                a_host.get(r, c),
-                b_host.get(r, c),
+                a_host[c * a.height() + r],
+                b_host[c * b.height() + r],
                 "Mismatch at row {} column {}",
                 r,
                 c
@@ -155,11 +155,11 @@ pub fn assert_eq_host_and_device_matrix<T: Clone + Send + Sync + PartialEq + Deb
 ) {
     assert_eq!(gpu.width(), cpu.width());
     assert_eq!(gpu.height(), cpu.height());
-    let gpu = transport_device_matrix_to_host(gpu);
+    let gpu = gpu.to_host().unwrap();
     for r in 0..cpu.height() {
         for c in 0..cpu.width() {
             assert_eq!(
-                gpu.get(r, c),
+                gpu[c * cpu.height() + r],
                 cpu.get(r, c),
                 "Mismatch at row {} column {}",
                 r,
@@ -167,11 +167,4 @@ pub fn assert_eq_host_and_device_matrix<T: Clone + Send + Sync + PartialEq + Deb
             );
         }
     }
-}
-
-// Debug utility to print out a DeviceMatrix as a RowMajorMatrix for comparison
-pub fn print_device_matrix_as_row_major_matrix<T: Clone + Send + Sync + Debug>(
-    gpu_matrix: &DeviceMatrix<T>,
-) {
-    println!("{:?}", transport_device_matrix_to_host(gpu_matrix));
 }
