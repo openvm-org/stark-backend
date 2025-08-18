933154302ca224e1658de4e0ccbd1efdfa5d8448
Grigorii Alekseev <grigorii@intrinsictech.xyz>
2025-08-18 21:50:22 +0000

    shorter

--- crates/cuda-common/build.rs
+++ crates/cuda-common/build.rs
@@ -4,9 +4,8 @@ use openvm_cuda_builder::{cuda_available, CudaBuilder};
 
 fn main() {
     if cuda_available() {
-        let builder = CudaBuilder::new();
-        builder.emit_link_directives();
-        
+        CudaBuilder::new().emit_link_directives();
+
         let include_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("include");
         println!("cargo:include={}", include_path.display()); // -> DEP_CUDA_COMMON_INCLUDE
         println!("cargo:rerun-if-changed={}", include_path.display());
