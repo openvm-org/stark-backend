2ec77edbd0c0796a360d5e57660839909af83410
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-08-29 17:46:20 -0700

    Revert "chore(cuda): Sync when release big buffer & bump version to `1.2.0-rc.8`" (#113)
    Reverts openvm-org/stark-backend#111
    
    Performance regression in pcs opening:
    https://github.com/axiom-crypto/openvm-reth-benchmark/actions/runs/17335892603

--- crates/cuda-common/src/common.rs
+++ crates/cuda-common/src/common.rs
@@ -8,7 +8,6 @@ type cudaMemPool_t = *mut c_void;
 pub const cudaMemPoolAttrReleaseThreshold: u32 = 4;
 #[allow(non_upper_case_globals)]
 pub const cudaMemPoolAttrReuseFollowEventDependencies: u32 = 1;
-pub const MEMORY_POOL_SIZE: usize = 512 * 1024 * 1024;
 
 #[link(name = "cudart")]
 extern "C" {
@@ -47,10 +46,11 @@ pub fn set_device() -> Result<(), CudaError> {
         ))?;
 
         // 2. Set release threshold to 512 MB
+        let threshold: usize = 512 * 1024 * 1024;
         check(cudaMemPoolSetAttribute(
             pool,
             cudaMemPoolAttrReleaseThreshold,
-            &MEMORY_POOL_SIZE as *const usize as *const c_void,
+            &threshold as *const usize as *const c_void,
         ))?;
 
         // 3. Optional but safe: assign pool back to device
