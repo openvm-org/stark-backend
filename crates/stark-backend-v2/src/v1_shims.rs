4cf5875d7dd5f3d77968024e23432f799cae6f7e
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-11-06 23:17:24 -0800

    chore: `V1Compat` trait for cuda v1 shims (#172)
    Rust orphan rule forces me to make a trait.
    
    towards INT-5392

--- crates/stark-backend-v2/src/v1_shims.rs
+++ crates/stark-backend-v2/src/v1_shims.rs
@@ -4,23 +4,24 @@ use openvm_stark_backend::{
     Chip,
     keygen::types::{MultiStarkProvingKey, StarkProvingKey},
     prover::{
-        MatrixDimensions,
+        MatrixDimensions, ProverBackend,
         cpu::CpuBackend,
         types::{AirProvingContext, ProvingContext},
     },
 };
 use openvm_stark_sdk::config::baby_bear_poseidon2::BabyBearPoseidon2Config;
+use p3_matrix::dense::RowMajorMatrix;
 use p3_util::log2_strict_usize;
 
 use crate::{
-    ChipV2, SystemParams,
+    ChipV2, F, SystemParams,
     keygen::types::{
         MultiStarkProvingKeyV2, StarkProvingKeyV2, StarkVerifyingKeyV2, StarkVerifyingParamsV2,
         VerifierSinglePreprocessedData,
     },
     prover::{
-        AirProvingContextV2, ColMajorMatrix, CommittedTraceDataV2, CpuBackendV2, ProvingContextV2,
-        stacked_pcs::stacked_commit,
+        AirProvingContextV2, ColMajorMatrix, CommittedTraceDataV2, CpuBackendV2, ProverBackendV2,
+        ProvingContextV2, stacked_pcs::stacked_commit,
     },
 };
 
@@ -86,46 +87,51 @@ impl StarkProvingKeyV2 {
     }
 }
 
-impl ProvingContextV2<CpuBackendV2> {
-    pub fn from_v1(params: SystemParams, ctx: ProvingContext<CpuBackend<SC>>) -> Self {
-        let per_air = ctx
+pub trait V1Compat: ProverBackendV2 {
+    type V1: ProverBackend<Val = <Self as ProverBackendV2>::Val>;
+
+    fn convert_trace(matrix: <Self::V1 as ProverBackend>::Matrix) -> Self::Matrix;
+
+    fn convert_pcs_data(
+        params: SystemParams,
+        matrix: <Self::V1 as ProverBackend>::Matrix,
+    ) -> (Self::Commitment, Self::PcsData);
+}
+
+impl<PB: V1Compat> ProvingContextV2<PB> {
+    pub fn from_v1(params: SystemParams, ctx: ProvingContext<PB::V1>) -> Self {
+        let per_trace = ctx
             .per_air
             .into_iter()
-            .map(|(air_id, air_ctx)| (air_id, AirProvingContextV2::from_v1(params, air_ctx)))
+            .map(|(air_idx, air_ctx)| (air_idx, AirProvingContextV2::from_v1(params, air_ctx)))
             .collect();
-        Self { per_trace: per_air }
+        Self::new(per_trace)
     }
 
-    pub fn from_v1_no_cached(ctx: ProvingContext<CpuBackend<SC>>) -> Self {
-        let per_air = ctx
+    pub fn from_v1_no_cached(ctx: ProvingContext<PB::V1>) -> Self {
+        let per_trace = ctx
             .per_air
             .into_iter()
-            .map(|(air_id, air_ctx)| (air_id, AirProvingContextV2::from_v1_no_cached(air_ctx)))
+            .map(|(air_idx, air_ctx)| (air_idx, AirProvingContextV2::from_v1_no_cached(air_ctx)))
             .collect();
-        Self { per_trace: per_air }
+        Self::new(per_trace)
     }
 }
 
-impl AirProvingContextV2<CpuBackendV2> {
-    pub fn from_v1(params: SystemParams, ctx: AirProvingContext<CpuBackend<SC>>) -> Self {
+impl<PB: V1Compat> AirProvingContextV2<PB> {
+    pub fn from_v1(params: SystemParams, ctx: AirProvingContext<PB::V1>) -> Self {
         let common_main =
-            ColMajorMatrix::from_row_major(&ctx.common_main.expect("must have common main"));
+            <PB as V1Compat>::convert_trace(ctx.common_main.expect("must have common main"));
         let cached_mains = ctx
             .cached_mains
-            .iter()
+            .into_iter()
             .map(|d| {
-                let trace = ColMajorMatrix::from_row_major(&d.trace);
-                let (commitment, data) = stacked_commit(
-                    params.l_skip,
-                    params.n_stack,
-                    params.log_blowup,
-                    params.k_whir,
-                    &[&trace],
-                );
+                let height = d.trace.height();
+                let (commitment, data) = <PB as V1Compat>::convert_pcs_data(params, d.trace);
                 CommittedTraceDataV2 {
                     commitment,
                     data: Arc::new(data),
-                    height: trace.height(),
+                    height,
                 }
             })
             .collect();
@@ -136,10 +142,10 @@ impl AirProvingContextV2<CpuBackendV2> {
         }
     }
 
-    pub fn from_v1_no_cached(ctx: AirProvingContext<CpuBackend<SC>>) -> Self {
-        let common_main =
-            ColMajorMatrix::from_row_major(&ctx.common_main.expect("must have common main"));
+    pub fn from_v1_no_cached(ctx: AirProvingContext<PB::V1>) -> Self {
         assert!(ctx.cached_mains.is_empty());
+        let common_main =
+            <PB as V1Compat>::convert_trace(ctx.common_main.expect("must have common main"));
         Self {
             cached_mains: vec![],
             common_main,
@@ -148,12 +154,34 @@ impl AirProvingContextV2<CpuBackendV2> {
     }
 }
 
-impl<C, R> ChipV2<R, CpuBackendV2> for C
+impl<C, R, PB: V1Compat> ChipV2<R, PB> for C
 where
-    C: Chip<R, CpuBackend<SC>>,
+    C: Chip<R, PB::V1>,
 {
-    fn generate_proving_ctx(&self, records: R) -> AirProvingContextV2<CpuBackendV2> {
+    fn generate_proving_ctx(&self, records: R) -> AirProvingContextV2<PB> {
         let v1_ctx = self.generate_proving_ctx(records);
         AirProvingContextV2::from_v1_no_cached(v1_ctx)
+    }
+}
+
+impl V1Compat for CpuBackendV2 {
+    type V1 = CpuBackend<SC>;
+
+    fn convert_trace(matrix: Arc<RowMajorMatrix<F>>) -> Self::Matrix {
+        ColMajorMatrix::from_row_major(&matrix)
+    }
+
+    fn convert_pcs_data(
+        params: SystemParams,
+        matrix: Arc<RowMajorMatrix<F>>,
+    ) -> (Self::Commitment, Self::PcsData) {
+        let trace = ColMajorMatrix::from_row_major(&matrix);
+        stacked_commit(
+            params.l_skip,
+            params.n_stack,
+            params.log_blowup,
+            params.k_whir,
+            &[&trace],
+        )
     }
 }
