72443fb1cb76de32e9d143902694738f21e30c46
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-11-29 20:06:15 -0800

    feat: add v2 keygen (#183)
    - Added `LogupSecurityParams` and `max_constraint_degree` to
    `SystemParams` since they are both global configuration params.
    - `MultiStarkKeygenBuilderV2` now has a `add_required_air` function,
    although it is not used anywhere yet
    - `MultiStarkKeygenBuilderV2` generates the ProvingKeyV2 directly, and
    so `from_v1` functions have been deleted
    - New keygen is used directly in `EngineV2` (at present always using
    CPU)
    - Now it will return KeygenError if any AIR has constraint degree
    (including interactions) exceeding the configured max. The engine just
    unwraps the error at the moment - I made a followup ticket to add error
    types but didn't want to cause such a big diff right now.
    
    This doesn't add interaction chunking, but I'll do that in a follow up.
    
    closes INT-5142

--- crates/stark-backend-v2/src/verifier/mod.rs
+++ crates/stark-backend-v2/src/verifier/mod.rs
@@ -66,7 +66,6 @@ pub fn verify<TS: FiatShamirTranscript>(
         params,
         per_air,
         trace_height_constraints,
-        max_constraint_degree: _,
     } = &mvk;
     let l_skip = params.l_skip;
 
@@ -189,7 +188,9 @@ pub fn verify<TS: FiatShamirTranscript>(
 
 #[cfg(test)]
 mod tests {
-    use openvm_stark_sdk::config::setup_tracing_with_log_level;
+    use openvm_stark_sdk::config::{
+        log_up_params::log_up_security_params_baby_bear_100_bits, setup_tracing_with_log_level,
+    };
     use test_case::test_case;
     use tracing::Level;
 
@@ -220,8 +221,9 @@ mod tests {
             k_whir,
             num_whir_queries: 100,
             log_final_poly_len,
-            logup_pow_bits: 1,
+            logup: log_up_security_params_baby_bear_100_bits(),
             whir_pow_bits: 1,
+            max_constraint_degree: 3,
         };
         let fib = FibFixture::new(0, 1, 1 << log_trace_degree);
 
