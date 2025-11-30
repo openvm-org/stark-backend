8e33da544bba25458dd7a260d578a6c58a39f0cd
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-11-29 20:06:15 -0800

    feat: add v2 keygen (#183)
    - Added `LogupSecurityParams` and `max_constraint_degree` to
    `SystemParams` since they are both global configuration params.
    - `MultiStarkKeygenBuilderV2` now has a `add_required_air` function,
    although it is not used anywhere yet
    - `MultiStarkKeygenBuilderV2` generates the ProvingKeyV2 directly, and
    so `from_v1` functions have been deleted
    - New keygen is used directly in `EngineV2` (at present always using
    CPU)
    - Now it will return KeygenError if any AIR has constraint degree
    (including interactions) exceeding the configured max. The engine just
    unwraps the error at the moment - I made a followup ticket to add error
    types but didn't want to cause such a big diff right now.
    
    This doesn't add interaction chunking, but I'll do that in a follow up.
    
    closes INT-5142

--- crates/stark-backend-v2/src/v1_shims.rs
+++ crates/stark-backend-v2/src/v1_shims.rs
@@ -1,23 +1,17 @@
 use std::sync::Arc;
 
 use openvm_stark_backend::{
-    keygen::types::{MultiStarkProvingKey, StarkProvingKey},
     prover::{
         cpu::CpuBackend,
         types::{AirProvingContext, ProvingContext},
-        MatrixDimensions, ProverBackend,
+        ProverBackend,
     },
     Chip,
 };
 use openvm_stark_sdk::config::baby_bear_poseidon2::BabyBearPoseidon2Config;
 use p3_matrix::dense::RowMajorMatrix;
-use p3_util::log2_strict_usize;
 
 use crate::{
-    keygen::types::{
-        find_unused_vars, MultiStarkProvingKeyV2, StarkProvingKeyV2, StarkVerifyingKeyV2,
-        StarkVerifyingParamsV2, VerifierSinglePreprocessedData,
-    },
     prover::{
         stacked_pcs::stacked_commit, AirProvingContextV2, ColMajorMatrix, CommittedTraceDataV2,
         CpuBackendV2, ProverBackendV2, ProvingContextV2,
@@ -26,68 +20,6 @@ use crate::{
 };
 
 type SC = BabyBearPoseidon2Config;
-
-impl MultiStarkProvingKeyV2 {
-    pub fn from_v1(params: SystemParams, pk: MultiStarkProvingKey<SC>) -> Self {
-        let per_air = pk
-            .per_air
-            .into_iter()
-            .map(|pk| StarkProvingKeyV2::from_v1(params, pk))
-            .collect();
-        let trace_height_constraints = pk.trace_height_constraints;
-        let max_constraint_degree = pk.max_constraint_degree;
-
-        Self {
-            per_air,
-            trace_height_constraints,
-            max_constraint_degree,
-            params,
-            vk_pre_hash: pk.vk_pre_hash.into(),
-        }
-    }
-}
-
-impl StarkProvingKeyV2 {
-    pub fn from_v1(params: SystemParams, pk: StarkProvingKey<SC>) -> Self {
-        // If preprocessed trace exists, re-commit using stacked PCS
-        let mut preprocessed_vdata = None;
-        let preprocessed_data = pk.preprocessed_data.map(|d| {
-            let trace = ColMajorMatrix::from_row_major(&d.trace);
-            let (commit, data) = stacked_commit(
-                params.l_skip,
-                params.n_stack,
-                params.log_blowup,
-                params.k_whir,
-                &[&trace],
-            );
-            preprocessed_vdata = Some(VerifierSinglePreprocessedData {
-                commit,
-                hypercube_dim: log2_strict_usize(trace.height()) as isize - params.l_skip as isize,
-                stacking_width: data.matrix.width(),
-            });
-            Arc::new(data)
-        });
-        let vparams = StarkVerifyingParamsV2 {
-            width: pk.vk.params.width,
-            num_public_values: pk.vk.params.num_public_values,
-        };
-        let symbolic_constraints = pk.vk.symbolic_constraints;
-        let unused_variables = find_unused_vars(&symbolic_constraints, &vparams.width);
-        let vk = StarkVerifyingKeyV2 {
-            preprocessed_data: preprocessed_vdata,
-            params: vparams,
-            symbolic_constraints,
-            max_constraint_degree: pk.vk.max_constraint_degree,
-            is_required: false, // no AIRs are required in v1
-            unused_variables,
-        };
-        Self {
-            air_name: pk.air_name,
-            vk,
-            preprocessed_data,
-        }
-    }
-}
 
 pub trait V1Compat: ProverBackendV2 + Sized {
     type V1: ProverBackend<Val = <Self as ProverBackendV2>::Val>;
