8798d199fa3180dd1684fd6bf9faa5cadb207bda
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2026-03-03 11:30:56 -1000

    init code copy

---
+++ crates/stark-backend/src/keygen/lean.rs
@@ -0,0 +1,5 @@
+//! Extraction of AIR constraints (with interactions) from symbolic DAGs to Lean4 code.
+//! This extraction can be used for standalone Lean4 code generation from serialized circuit
+//! verifying keys.
+//
+// Original extraction code was introduced in Nethermind fork <https://github.com/NethermindEth/openvm-stark-backend/releases/tag/v1.2.2-extraction>
